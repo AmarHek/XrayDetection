309d15f0500fade39948ff9446fc0d02e7d129b5
AmarHek <amar.hekalo@uni-wuerzburg.de>
2024-05-24 15:16:53 +0200

    hyperparameter update

--- src/train.py
+++ src/train.py
@@ -1,19 +1,33 @@
 import os
 from ultralytics import YOLO
 
-# Training parameters
-EPOCHS = 100
-IMG_SIZE = 640
-BATCH_SIZE = 16
-PATIENCE = 10
-SAVE = True
-SAVE_PERIOD = 2
 PROJECT = "vindr_yolov8"
 name = "initial_run"
 
+# Training parameters
+EPOCHS = 100
+IMG_SIZE = 640
+PATIENCE = 10
+BATCH_SIZE = 16
+SAVE = True
+LR0 = 0.01
+LRF = 0.01
+
+# Augmentation parameters
+HSV_H = 0
+HSV_S = 0
+HSV_V = 0
+DEGREES = 10
+TRANSLATE = 0.1
+SCALE = 0.05
+FLIPLR = 0.5
+FLIPUP = 0
+ERASING = 0.05
+CROP_FRACTION = 0.9
+
+
 config_path = "../configs/"
 data_config = os.path.join(config_path, "data.yaml")
-hyp_config = os.path.join(config_path, "hyp.yaml")
 
 # Load the model
 model = YOLO("yolov8m.pt")
@@ -23,11 +37,10 @@ model.info()
 
 # Train the model
 results = model.train(data=data_config,
-                      epochs=EPOCHS,
-                      batch=BATCH_SIZE,
-                      imgsz=IMG_SIZE,
-                      SAVE=SAVE,
-                      project=PROJECT,
-                      save_period=SAVE_PERIOD,
                       device="cuda",
-                      hyp=hyp_config)
+                      project=PROJECT, name=name,
+                      epochs=EPOCHS, batch_size=BATCH_SIZE, imgsz=IMG_SIZE, save=SAVE,
+                      patience=PATIENCE, lr0=LR0, lrf=LRF,
+                      hsv_h=HSV_H, hsv_s=HSV_S, hsv_v=HSV_V,
+                      degrees=DEGREES, translate=TRANSLATE, scale=SCALE,
+                      fliplr=FLIPLR, flipud=FLIPUP)

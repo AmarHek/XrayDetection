d742459d5c18a7f8c1b0bee3b7a78829a4b95663
AmarHek <amar.hekalo@uni-wuerzburg.de>
2024-05-24 14:55:07 +0200

    hyperparameter update

--- src/train.py
+++ src/train.py
@@ -4,27 +4,16 @@ from ultralytics import YOLO
 # Training parameters
 EPOCHS = 100
 IMG_SIZE = 640
-PATIENCE = 10
 BATCH_SIZE = 16
+PATIENCE = 10
 SAVE = True
-LR0 = 0.01
-LRF = 0.01
-
-# Augmentation parameters
-HSV_H = 0
-HSV_S = 0
-HSV_V = 0
-DEGREES = 10
-TRANSLATE = 0.1
-SCALE = 0.05
-FLIPLR = 0.5
-FLIPUP = 0
-ERASING = 0.05
-CROP_FRACTION = 0.9
-
+SAVE_PERIOD = 2
+PROJECT = "vindr_yolov8"
+name = "initial_run"
 
 config_path = "../configs/"
 data_config = os.path.join(config_path, "data.yaml")
+hyp_config = os.path.join(config_path, "hyp.yaml")
 
 # Load the model
 model = YOLO("yolov8m.pt")
@@ -34,9 +23,11 @@ model.info()
 
 # Train the model
 results = model.train(data=data_config,
+                      epochs=EPOCHS,
+                      batch=BATCH_SIZE,
+                      imgsz=IMG_SIZE,
+                      SAVE=SAVE,
+                      project=PROJECT,
+                      save_period=SAVE_PERIOD,
                       device="cuda",
-                      epochs=EPOCHS, batch_size=BATCH_SIZE, imgsz=IMG_SIZE, save=SAVE,
-                      patience=PATIENCE, lr0=LR0, lrf=LRF,
-                      hsv_h=HSV_H, hsv_s=HSV_S, hsv_v=HSV_V,
-                      degrees=DEGREES, translate=TRANSLATE, scale=SCALE,
-                      fliplr=FLIPLR, flipud=FLIPUP, crop_fraction=CROP_FRACTION)
+                      hyp=hyp_config)

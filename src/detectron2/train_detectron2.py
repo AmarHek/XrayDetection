0c57dca53a291c89370a73f82eb64449d9d5e603
AmarHek <amar.hekalo@uni-wuerzburg.de>
2024-07-17 21:02:51 +0200

    adjust detectron2 annotations for new vindr dataset

--- src/detectron2/train_detectron2.py
+++ src/detectron2/train_detectron2.py
@@ -22,36 +22,37 @@ train_dir = os.path.join(image_dir, "train")
 val_dir = os.path.join(image_dir, "val")
 test_dir = os.path.join(image_dir, "test")
 
+# parameters
+model_config = "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
+batch_size = 8
+num_workers = 2
+lr = 0.00025
+max_iter = 50000
+batch_size_per_image = 128
+num_classes = 14
+
 # Register your datasets
 register_coco_instances("vindr_train", {}, train_json, train_dir)
 register_coco_instances("vindr_val", {}, val_json, val_dir)
 register_coco_instances("vindr_test", {}, test_json, test_dir)
 
 # Verify the registration
-my_dataset_train_metadata = MetadataCatalog.get("vinDr_train")
+vindr_train_metadata = MetadataCatalog.get("vinDr_train")
 dataset_dicts = DatasetCatalog.get("vinDr_train")
 
-# Visualize the dataset (optional)
-for d in random.sample(dataset_dicts, 3):
-    img = cv2.imread(d["file_name"])
-    visualizer = Visualizer(img[:, :, ::-1], metadata=my_dataset_train_metadata, scale=0.5)
-    vis = visualizer.draw_dataset_dict(d)
-    cv2.imshow('sample', vis.get_image()[:, :, ::-1])
-    cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 # Set up the configuration
 cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
-cfg.DATASETS.TRAIN = ("my_dataset_train",)
-cfg.DATASETS.TEST = ("my_dataset_val",)
-cfg.DATALOADER.NUM_WORKERS = 2
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")  # Use pre-trained weights
-cfg.SOLVER.IMS_PER_BATCH = 2
-cfg.SOLVER.BASE_LR = 0.00025  # Learning rate
-cfg.SOLVER.MAX_ITER = 1000    # Number of iterations
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3  # Number of classes in your dataset (update accordingly)
+cfg.merge_from_file(model_zoo.get_config_file(model_config))
+cfg.DATASETS.TRAIN = ("vindr_train",)
+cfg.DATASETS.TEST = ("vindr_val",)
+cfg.DATALOADER.NUM_WORKERS = num_workers
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_config)  # Use pre-trained weights
+cfg.SOLVER.IMS_PER_BATCH = batch_size
+cfg.SOLVER.BASE_LR = lr  # Learning rate
+cfg.SOLVER.MAX_ITER = max_iter    # Number of iterations
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = batch_size_per_image
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes  # Number of classes in your dataset (update accordingly)
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 
@@ -70,7 +71,7 @@ cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
 predictor = DefaultPredictor(cfg)
 
 v = Visualizer(image[:, :, ::-1],
-               metadata=my_dataset_train_metadata,
+               metadata=vindr_train_metadata,
                scale=0.5,
                instance_mode=ColorMode.SEGMENTATION)
 v = v.draw_instance_predictions(outputs["instances"].to("cpu"))

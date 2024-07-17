ffb207748ba2375d8a4821103bbec4d94a75279a
AmarHek <amar.hekalo@uni-wuerzburg.de>
2024-07-17 14:55:05 +0200

    adjust detectron2 annotations for new vindr dataset

--- src/detectron2/split_annotations_postprocess.py
+++ src/detectron2/split_annotations_postprocess.py
@@ -13,17 +13,15 @@ def save_json(data, json_file):
         json.dump(data, f)
 
 
-def split_annotations(annotation_file, train_dir, val_dir, test_dir, output_dir):
+def split_annotations(annotation_file, train_dir, val_dir, output_dir):
     coco_data = load_json(annotation_file)
 
     # Create dictionaries to hold new data
     train_data = {k: [] for k in coco_data if isinstance(coco_data[k], list)}
     val_data = {k: [] for k in coco_data if isinstance(coco_data[k], list)}
-    test_data = {k: [] for k in coco_data if isinstance(coco_data[k], list)}
 
     train_img_ids = set()
     val_img_ids = set()
-    test_img_ids = set()
 
     def get_image_ids(directory, img_ids):
         for filename in os.listdir(directory):
@@ -35,7 +33,6 @@ def split_annotations(annotation_file, train_dir, val_dir, test_dir, output_dir)
 
     get_image_ids(train_dir, train_img_ids)
     get_image_ids(val_dir, val_img_ids)
-    get_image_ids(test_dir, test_img_ids)
 
     def filter_data(img_ids, target_data):
         for img in coco_data['images']:
@@ -54,25 +51,21 @@ def split_annotations(annotation_file, train_dir, val_dir, test_dir, output_dir)
 
     filter_data(train_img_ids, train_data)
     filter_data(val_img_ids, val_data)
-    filter_data(test_img_ids, test_data)
 
     os.makedirs(output_dir, exist_ok=True)
     save_json(train_data, os.path.join(output_dir, 'instances_train.json'))
     save_json(val_data, os.path.join(output_dir, 'instances_val.json'))
-    save_json(test_data, os.path.join(output_dir, 'instances_test.json'))
 
     print(f"Saved {len(train_data['images'])} train images and {len(train_data['annotations'])} annotations.")
     print(f"Saved {len(val_data['images'])} val images and {len(val_data['annotations'])} annotations.")
-    print(f"Saved {len(test_data['images'])} test images and {len(test_data['annotations'])} annotations.")
 
 
 if __name__ == "__main__":
     root = "/scratch/hekalo/Datasets/vindr/"
-    annotations_file = os.path.join(root, "coco_annotations.json")
+    annotations_file = os.path.join(root, "coco_annotations_trainval.json")
     output_dir = os.path.join(root, "dataset", "annotations")
     image_dir = os.path.join(root, "dataset", "images")
     train_dir = os.path.join(image_dir, "train")
     val_dir = os.path.join(image_dir, "val")
-    test_dir = os.path.join(image_dir, "test")
 
-    split_annotations(annotations_file, train_dir, val_dir, test_dir, output_dir)
+    split_annotations(annotations_file, train_dir, val_dir, output_dir)
